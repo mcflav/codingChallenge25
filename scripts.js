07ed9f80bb6b7a9b82361ede11a11fddb259bbec
mcflav <52865683+mcflav@users.noreply.github.com>
2020-01-17 02:03:54 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,13 +2,13 @@ function PriceServices(){
    var service = document.getElementById("services");
    var strService = service.options[service.selectedIndex].text;
     
-   if(strService === "mowing"){
+   if(strService === "Lawn Mowing"){
      document.getElementById("price").innerHTML = "$40.00"
-   }else if(strService === "trimming"){
+   }else if(strService === "Bush Trimming"){
      document.getElementById("price").innerHTML = "$25.00"
-   }else if(strService === "mulch"){
+   }else if(strService === "Laying Mulch"){
      document.getElementById("price").innerHTML = "$35.00"
-   }else if(strService === "leaf"){
+   }else if(strService === "Leaf Removal"){
      document.getElementById("price").innerHTML = "25.00"
    }
 }
