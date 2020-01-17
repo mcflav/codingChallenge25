4d35d53d4744886a85e1e133b5e1f9a33c6a57a1
mcflav <52865683+mcflav@users.noreply.github.com>
2020-01-17 00:09:07 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,16 @@
 window.onload = PriceServices;
 
 function PriceServices(){
+   var service = document.getElementById('services');
+   var price = document.getElementsByName("pricing")[0].tagName;
 
+   If(service.value === "mowing"){
+
+} else if(service.value === "trimming"){
+
+} else if(service.value === "mulch"){
+
+} else if(service.value === "leaf"){
+
+}
 }
